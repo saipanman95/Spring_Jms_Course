f2845dcfe3ff26964962ab4393dd086dfcfc8686
Michael D Rodgers <michael.d.rodgers.jr@gmail.com>
2018-05-30 15:32:50 -0400

    Update Application.java
    made changes as noted by Kealy Slupsky in Beta Testing to match video files

--- Chapter_01/01_03_begin/src/main/java/com/mdrsolutions/SpringJmsExample01/Application.java
+++ Chapter_01/01_03_begin/src/main/java/com/mdrsolutions/SpringJmsExample01/Application.java
@@ -2,12 +2,26 @@ package com.mdrsolutions.SpringJmsExample01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotion.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.ConnectionFactory;
+
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+		
+		
 	}
 
 }
